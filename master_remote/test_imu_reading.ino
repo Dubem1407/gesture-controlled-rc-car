27d1ad5e6761950ea1790427b99c81002f6c2c11
Chidubem Emeka-Nwuba <dubem1407@gmail.com>
2026-07-26 18:39:58 -0600

    Translated IMU readings correctly to give directional states

--- master_remote/test_imu_reading.ino
+++ master_remote/test_imu_reading.ino
@@ -7,12 +7,26 @@ calData calib = { 0 };
 AccelData accelData;
 GyroData gyroData;
 
+// Angles
+float accAngleX, accAngleY;
+float roll = 0, pitch = 0;
+
+// Gyro Offsets 
+float gyroX_offset = 1.85;
+float gyroY_offset = 1.37;
+float gyroZ_offset = 0.03;
+
+// Time Vars
+unsigned long previousTime = 0;
+float elapsedTime = 0;
+unsigned long prevPrintTime = 0;
+const unsigned long PRINT_INTERVAL = 1000; // print every 1s
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Wire.begin();
   delay(1000);
 
-  // Check for init error
   int err = IMU.init(calib, 0x68);
   if (err != 0) {
     Serial.print("IMU init failed. Error code: ");
@@ -20,24 +34,64 @@ void setup() {
     while (1);
   }
   Serial.println("MPU6500 ready.");
+
+  previousTime = millis();
 }
 
 void loop() {
+  // calculate delta time
+  unsigned long currentTime = millis();
+  elapsedTime = (currentTime - previousTime) / 1000.0; // convert to seconds
+  previousTime = currentTime;
+
+  // get sensor data
   IMU.update();
   IMU.getAccel(&accelData);
-  IMU.getGyro(&gyroData);   
+  IMU.getGyro(&gyroData);
 
-  // Read and Print IMU readings 
-  Serial.print("Accel X: "); Serial.print(accelData.accelX);
-  Serial.print("  Y: ");     Serial.print(accelData.accelY);
-  Serial.print("  Z: ");     Serial.println(accelData.accelZ);
+  float AccX = accelData.accelX;
+  float AccY = accelData.accelY;
+  float AccZ = accelData.accelZ;
 
-  Serial.print("Gyro  X: "); Serial.print(gyroData.gyroX);
-  Serial.print("  Y: ");     Serial.print(gyroData.gyroY);
-  Serial.print("  Z:");     Serial.println(gyroData.gyroZ);
+  // apply calibrations 
+  float GyroX = gyroData.gyroX - gyroX_offset;
+  float GyroY = gyroData.gyroY - gyroY_offset;
+  float GyroZ = gyroData.gyroZ - gyroZ_offset;
 
-  Serial.println("---");
-  delay(500);
+  // calculate tilt angles and apply filters
+  accAngleX = (atan(AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180.0 / PI);
+  accAngleY = (atan(-1 * AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180.0 / PI);
+
+  roll  = 0.96 * (roll + GyroY * elapsedTime) + 0.04 * accAngleX; // For left/right tilt
+  pitch = 0.96 * (pitch + (-GyroX) * elapsedTime) + 0.04 * accAngleY; // For up/down tilt
+
+  // Print results
+  if (currentTime - prevPrintTime >= PRINT_INTERVAL) {
+    prevPrintTime = currentTime; // Reset timer
+
+    // print pitch and roll results
+    Serial.print("Pitch: ");
+    Serial.print(pitch);
+    Serial.print("  |  Roll: ");
+    Serial.print(roll);
+    Serial.print("  --> State: ");
+
+    // apply thresholds and print corresponding state 
+    if (pitch > 20.0) {
+      Serial.println("Tilted UP");
+    } else if (pitch < -20.0) {
+      Serial.println("Tilted DOWN");
+    } else if (roll > 20.0) {
+      Serial.println("Tilted RIGHT");
+    } else if (roll < -20.0) {
+      Serial.println("Tilted LEFT");
+    } else {
+      Serial.println("FLAT / LEVEL");
+    }
+  }
+
+  delay(20);
+
 
   // TODO: 
   // Caliberate/test tilting angles to see what is considered up, down, right or left
